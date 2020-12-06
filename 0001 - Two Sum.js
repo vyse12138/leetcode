21c946c97e3fdefec428e99250124e30b84aead4
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-06 16:57:45 +1100

    Update 0001 - Two Sum.js

--- 0001 - Two Sum.js
+++ 0001 - Two Sum.js
@@ -3,7 +3,7 @@ const twoSum = (nums, target)=> {
     for (let i = 0; i < nums.length; i++){
         //store the value of the first parameter 
         let firstNum = nums[i];
-        //for loop the nums array starting after the first parameter
+        //for loop the rest nums array
         for (let j = i + 1; j < nums.length; j++){
             //return when targets are found
             if(target - firstNum == nums[j]){
