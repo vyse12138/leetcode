5f75fbfd9dea700d0cd3a633dfc7e225bc1c980d
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-06 16:55:42 +1100

    Update 0003 - Longest Substring Without Repeating Characters.js

--- 0003 - Longest Substring Without Repeating Characters.js
+++ 0003 - Longest Substring Without Repeating Characters.js
@@ -1,6 +1,14 @@
+//      sliding window with a set to store occured character
+// 
+// 1.   check if character at right pointer has occured,
+//      if so, keep moving forward left pointer 
+//      until character at right pointer is no longer occured.
+// 2.   proceed right pointer in each iteration.
+// 3.   compare the current substring length with the longest substring.
+
 const lengthOfLongestSubstring = (s) => {
 
-    //set used to stored occured char
+    // set used to stored occured char
     const hasOccuredSet = new Set();
     let size = s.length,
     leftPointer = 0,
@@ -8,17 +16,17 @@ const lengthOfLongestSubstring = (s) => {
     result = 0;
 
     while (rightPointer < size){
-        //if the char at right pointer has occured, move left pointer by one and 
-        //delete the char at left pointer until no char is the same as the char at right pointer
+        // if the char at right pointer has occured, move left pointer by one and 
+        // delete the char at left pointer until no char is the same as the char at right pointer
         while (hasOccuredSet.has(s[rightPointer])){
             hasOccuredSet.delete(s[leftPointer])
             leftPointer++;
         }
-        //add the char at the right pointer to the hasOccuredSet and 
-        //increse right pointer by 1
+        // add the char at the right pointer to the hasOccuredSet and 
+        // increse right pointer by 1
         hasOccuredSet.add(s[rightPointer]);
         rightPointer++;
-        //compare and stoer the largest length to result
+        // compare and stoer the largest length to result
         result = Math.max(result, rightPointer-leftPointer);
 
     }
