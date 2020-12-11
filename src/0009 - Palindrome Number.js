ac570dbd59252d126af214acffc3399d562c51d4
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-11 21:41:00 +1100

    Update 0009 - Palindrome Number.js

--- src/0009 - Palindrome Number.js
+++ src/0009 - Palindrome Number.js
@@ -1,6 +1,8 @@
 const isPalindrome = (x) => {
     if (x >= 0){
+        //reverse the input
         let reverse = x.toString().split('').reverse().join('');
+        //check if reverse is the same as original input
         return (reverse == x) ? true : false;
     }
     return false;
