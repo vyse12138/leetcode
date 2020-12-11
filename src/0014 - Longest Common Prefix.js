469d10d9800fd721a4244a738f94c49f922e7386
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-11 21:59:26 +1100

    Update 0014 - Longest Common Prefix.js

---
+++ src/0014 - Longest Common Prefix.js
@@ -0,0 +1,24 @@
+const longestCommonPrefix = (strs) => {
+    //if strs is empty or the first string in strs is empty, 
+    //return empty string
+    if (strs.length === 0 || strs[0].length ===0 ){
+        return '';
+    }
+
+    let prefix = '', char = '';
+
+    //iterate each char in the first string 
+    for (let i=0; i<strs[0].length; i++){
+        char = strs[0].charAt(i)
+        //iterate each string strs after the first string
+        for (let j=1; j<strs.length; j++){
+            //if the prefix is not the same or empty string encountered,
+            //return current prefix
+            if (strs[j].length === 0  || char !== strs[j].charAt(i)) {
+                return prefix;
+            }
+        }
+        prefix += char;
+    }
+    return prefix;
+};
