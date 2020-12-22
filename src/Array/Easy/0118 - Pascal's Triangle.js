95b397cf3d12ed5946f43c8562f52e3d4cfb3f50
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-22 12:02:27 +1100

    pascal's triangle

---
+++ src/Array/Easy/0118 - Pascal's Triangle.js
@@ -0,0 +1,13 @@
+var generate = function(numRows) {
+    let result = [];
+    for (let i = 0; i < numRows; i++){
+        let row = new Array(i+1).fill(1);
+        if (i > 1){
+            for (j = 1; j < row.length-1; j++){
+                row[j] = result[i-1][j-1] + result[i-1][j];
+            }
+        }
+        result.push(row)
+    }
+    return result;
+};
---
+++ src/Array/Easy/0119 - Pascal's Triangle II.js
@@ -0,0 +1,13 @@
+var getRow = function(rowIndex) {
+    let result = [];
+    for (let i = 0; i < rowIndex + 1; i++){
+        let row = new Array(i+1).fill(1);
+        if (i > 1){
+            for (j = 1; j < row.length-1; j++){
+                row[j] = result[i-1][j-1] + result[i-1][j];
+            }
+        }
+        result.push(row)
+    }
+    return result[rowIndex];
+};
