3f41f9adcf70ed519303e1d2668193788d8754c8
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-22 19:21:35 +1100

    Create 0228 - Summary Ranges.js

---
+++ src/Array/Easy/0228 - Summary Ranges.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    //pointing at starting index of a range
+    let start = 0;
+    let result = [];
+    for (let i = 1; i < nums.length + 1; i++){
+        //if not counting continuously
+        if (nums[i] - nums[i-1] !== 1){
+            //push range to result
+            if ((i-1) - start === 0){
+                result.push(nums[start].toString());
+            }
+            else {
+                result.push(`${nums[start]}->${nums[i-1]}`);
+            }
+            //set the start index
+            start = i;
+        }
+    }
+    return result
+};
