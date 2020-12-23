b1bdf26c2a09624a2a34622b48ce12ca5f90936e
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-23 11:54:45 +1100

    Create 0485 - Max Consecutive Ones.js

---
+++ src/Array/Easy/0485 - Max Consecutive Ones.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/*-----solution1-----
+set a variable to store the starting index of consecutive one,
+iterate the array and move the starting index when 0 is encountered,
+compare and store the max length of consecutive one.
+Time complexity is O(n)
+*/
+var findMaxConsecutiveOnes = function(nums) {
+    let start = 0, result = 0;
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] === 0){
+            start = i+1;
+        }
+        result = Math.max(result, i-start+1)
+    }
+    return result;
+};
+
+
+/*-----solution2-----
+Similar to soultion1 but use a counter instead.
+Compare would only be used when 0 encoutnered.
+Time complexity is O(n)
+*/
+var findMaxConsecutiveOnes = function(nums) {
+   let result = 0, count = 0;
+   for (let i = 0; i < nums.length; i++){
+       if (nums[i] === 0){
+           count = 0;
+           result = Math.max(result, count)
+       }
+       else {
+           count++; 
+       }
+   }
+   return result;
+};
