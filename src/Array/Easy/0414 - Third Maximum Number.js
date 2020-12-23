22d878b60dfe3786957b072159c83f6163b01ef8
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-23 11:39:45 +1100

    Update 0414 - Third Maximum Number.js

--- src/Array/Easy/0414 - Third Maximum Number.js
+++ src/Array/Easy/0414 - Third Maximum Number.js
@@ -8,6 +8,7 @@ define 3 variables representing the first, second and the third max integer in t
 initializing them with the minimal safe integer,
 iterate through the list and compare the element with the 3 max integer,
 return the third max if exsit, else return the first max.
+Time complexity is O(n)
 */
 var thirdMax = function(nums) {
     let max1 = Number.MIN_SAFE_INTEGER, max2 = Number.MIN_SAFE_INTEGER, max3 = Number.MIN_SAFE_INTEGER;
