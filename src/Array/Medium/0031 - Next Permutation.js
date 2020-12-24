1115e6269e8bd813275c4c122856873348586f6b
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-24 12:40:11 +1100

    Update 0031 - Next Permutation.js

--- src/Array/Medium/0031 - Next Permutation.js
+++ src/Array/Medium/0031 - Next Permutation.js
@@ -5,18 +5,34 @@
 
 /*-----solution-----
 */
-var nextPermutation = function(nums) {
-    let index1 = -1, index2 = -1, temp;
-    for (let i = nums.length -1; i >= 0; i--){
-        if (nums[i] < nums[i-1] && index1 === -1){
-            index1 = i;
-        }
-        else if (nums[i] < index1 ) {
-            [nums[i], nums[index1]] = [nums[index1], nums[i]]
-            nums = nums.slice(0,i).concat(nums.slice(i+1, index1).reverse())
-            return nums;
+var nextPermutation = function (nums) {
+    let index1 = -1, index2 = nums.length - 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[i] > nums[i - 1]) {
+            index1 = i - 1;
+            break;
         }
     }
+    if (index1 !== -1) {
+        for (let i = nums.length - 1; i > index1; i--) {
+            if (nums[i] > nums[index1]) {
+                index2 = Math.min(index2, i);
+            }
+        }
+
+        [nums[index2], nums[index1]] = [nums[index1], nums[index2]]
+    }
+
+
+    index2 = nums.length - 1
+    index1++
+    while (index1 < index2) {
+        [nums[index2], nums[index1]] = [nums[index1], nums[index2]]
+        index1++;
+        index2--;
+    }
     return nums;
-    
+
 };
+
+
