44468a5310420f5c965e61779656ebd013e1036f
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-24 17:26:00 +1100

    Update 0031 - Next Permutation.js

--- src/Array/Medium/0031 - Next Permutation.js
+++ src/Array/Medium/0031 - Next Permutation.js
@@ -4,19 +4,33 @@
  */
 
 /*-----solution-----
+    3 step to find the next permutation
+1.  Iterate from right until the number is not increasing,
+    store the position in index1 and break the iteration.
+2.  Find the smallest number in all nmumbers that are larger than index1,
+    and store the position in index2, then swap them.
+3.  Reverse the sub array from index1 + 1 to the end.
+
+e.g.[1, 4, 5, 3, 2]
+1.  Index1 is 1 which is pointing at number nums[1] = 4
+2.  Find index2 to be 2, which is nums[2] = 5,
+    swap them, the array become [1, 5, 4 , 3, 2]
+3.  Reverse [4, 3, 2] in the array,
+    then the array become [1, 5, 2, 3, 4]
 */
 var nextPermutation = function (nums) {
-    let index1 = -1, index2 = nums.length - 1;
+    let index1 = -1, index2;
     for (let i = nums.length - 1; i >= 0; i--) {
         if (nums[i] > nums[i - 1]) {
             index1 = i - 1;
+            index2 = i;
             break;
         }
     }
     if (index1 !== -1) {
-        for (let i = nums.length - 1; i > index1; i--) {
-            if (nums[i] > nums[index1]) {
-                index2 = Math.min(index2, i);
+        for (let i = index1 + 1; i < nums.length ; i++) {
+            if (nums[i] > nums[index1] && nums[i] <= nums[index2]) {
+                index2 = i;
             }
         }
 
