5e26a6603a514a0090e8862bb0cf262f5aa21fc8
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-24 11:57:56 +1100

    Update 0031 - Next Permutation.js

--- src/Array/Medium/0031 - Next Permutation.js
+++ src/Array/Medium/0031 - Next Permutation.js
@@ -6,5 +6,17 @@
 /*-----solution-----
 */
 var nextPermutation = function(nums) {
+    let index1 = -1, index2 = -1, temp;
+    for (let i = nums.length -1; i >= 0; i--){
+        if (nums[i] < nums[i-1] && index1 === -1){
+            index1 = i;
+        }
+        else if (nums[i] < index1 ) {
+            [nums[i], nums[index1]] = [nums[index1], nums[i]]
+            nums = nums.slice(0,i).concat(nums.slice(i+1, index1).reverse())
+            return nums;
+        }
+    }
+    return nums;
     
 };
