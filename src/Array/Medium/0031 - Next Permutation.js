5c5ea999d22625ad20527f964bb6aec20ba009eb
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-24 11:23:05 +1100

    Create 0031 - Next Permutation.js

---
+++ src/Array/Medium/0031 - Next Permutation.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+/*-----solution-----
+*/
+var nextPermutation = function(nums) {
+    
+};
