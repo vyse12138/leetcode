dc14c11cecc5f248b8f78de13c3867b043129f97
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2020-12-27 18:27:52 +1100

    Update 0054 - Spiral Matrix.js

--- src/Array/Medium/0054 - Spiral Matrix.js
+++ src/Array/Medium/0054 - Spiral Matrix.js
@@ -3,24 +3,31 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    const size = matrix.length * matrix[0].length;
     let 
-        x = 0, 
-        y = 0, 
-        toRight = true,
-        toBot = true,
+        left = 0,
+        right = matrix[0].length -1,
+        top = 0,
+        bottom = matrix.length -1,
         result = [];
-
-    for(let i = 0; i < size; i++){
-        result.push(matrix[x][y]);
-        while (x < xLimit && toRight){
-            x++;
+    while (left <= right  && top <= bottom){
+        for (let i = left; i <= right; i++){
+            result.push(matrix[top][i]);
         }
-        
-        if (y < yLimit){
-            y++;
+        for (let i = top+1; i <= bottom; i++){
+            result.push(matrix[i][right]);
         }
-
+        if (left < right && top < bottom) {
+            for (let i = right-1; i > left; i--){
+                result.push(matrix[bottom][i]);
+            }
+            for (let i = bottom; i > top; i--){
+                result.push(matrix[i][left]);
+            }
+        }
+        left++;
+        right--;
+        top++;
+        bottom--;
     }
     return result;
 };
