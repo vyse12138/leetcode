c1cd2f8a44a7df5980c3319c18362230a624ae67
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-07 11:19:45 +1100

    Create 0189 - Rotate Array.js

---
+++ src/Array/Medium/0189 - Rotate Array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+/*-----solution 1-----
+Get the last element in the array and put it to the first,
+repeat k times.
+*/
+var rotate = function(nums, k) {
+    for (let i = 0; i < k; i++){
+      nums.unshift(nums.pop());
+    }
+};
+
+/*-----solution 2-----
+Get the last k elements in the array and put it to the first.
+Issue: when k > 2 * nums.length, it doesnt work
+*/
+var rotate = function(nums, k) {
+  nums.unshift(...nums.splice(nums.length - k, k));
+}; 
