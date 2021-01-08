6968a3f46214c85272cfb2499732045d8bf51618
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-08 11:57:46 +1100

    Create 0082 - Remove Duplicates from Sorted List II.js

---
+++ src/Linked List/Medium/0082 - Remove Duplicates from Sorted List II.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+/*-----solution-----
+Iterate the linked list, when repeated node shows,
+delete it. Otherwise, keep iterating.
+Time complexity is O(n)
+Space complexity is O(1)
+*/
+var deleteDuplicates = function (head) {
+  let dummyHead = ListNode(0,head),
+  current = head, 
+  previous = dummyHead,
+  duplicate = false;
+  while (current && current.next) {
+    duplicate = false;
+    while (current.val === current.next.val) {
+      current.next = current.next.next;
+      duplicate = true;
+    }
+    if (duplicate) {
+      previous.next = current.next;
+      current = current.next;
+    }
+    else {
+      previous = previous.next;
+      cur = cur.next;
+    }
+  }
+  return dummyHead.next;
+};
