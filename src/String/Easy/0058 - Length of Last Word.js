bacdec59625cf0d19858d00840ef56fcb3a67a68
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-13 12:42:11 +1100

    Create 0058 - Length of Last Word.js

---
+++ src/String/Easy/0058 - Length of Last Word.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/*----- solution 1 -----
+Convert to array and pop the last element.
+Time complexity is O(n^2)
+Space complexity is O(1)
+*/
+var lengthOfLastWord = function(s) {
+  return s.trim().split(' ').pop() ? s.trim().split(' ').pop().length : 0;
+};
