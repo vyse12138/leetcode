ae836776d29fb2a84a10e666aec8350d727811b9
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-14 17:14:04 +1100

    Create 0151 - Reverse Words in a String.js

---
+++ src/String/Medium/0151 - Reverse Words in a String.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+/*----- solution 1 -----
+Using APIs.
+Time complexity is O(n)
+Space complexity is O(n)
+*/
+var reverseWords = function(s) {
+  return s.trim().split(/\s+/g).reverse().join(' ');
+};
