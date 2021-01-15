e13552d2f2aacdd83e3e77d32fc2e0b9f437cc71
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-15 18:25:28 +1100

    Update 0049 - Group Anagrams.js

---
+++ src/String/Medium/0049 - Group Anagrams.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+
+/*----- solution 1 -----
+New hashmap, key is the sorted string,
+value is the list of strings that matches the sorted string.
+Iterate the strs array, put string into map.
+If key matches, put into it,
+otherwise, create a new key-value pair.
+Return values in hashmap.
+Time complexity is O(nmlog(m)) where m is the max length of string in strs
+Space complexity is O(mn)
+*/
+var groupAnagrams = function (strs) {
+  let map = new Map();
+  for (let str of strs) {
+    let key = str.split('').sort().join('');
+    let list = map.get(key) ? map.get(key) : new Array();
+    list.push(str);
+    map.set(key, list);
+  }
+  return Array.from(map.values());
+};
