913ff13fabd254a34a406833caf4c961da5f2a9e
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-17 18:15:07 +1100

    Create 0038 - Count and Say.js

---
+++ src/String/Easy/0038 - Count and Say.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+
+/*----- solution 1 -----
+Recursion.
+End when input is 1, return '1'.
+Compute count and say for previous node.
+Time complexity is O(n^2)
+Space complexity is O(n)
+*/
+var countAndSay = function(n) {
+  if (n === 1) {
+      return '1';
+  }
+  let previous = countAndSay(n-1), result='', count = 1;
+  for (let i = 0; i < previous.length; i++) {
+    if (previous[i] === previous[i+1]) {
+      count++;
+    }
+    else {
+      result += count.toString() + previous[i];
+      count = 1;
+    }
+  }
+  return result;
+};
