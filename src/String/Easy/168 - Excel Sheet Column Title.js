f179946b35abc41782d1d453d6acf0c58a360c9c
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-19 23:16:22 +1100

    Update 168 - Excel Sheet Column Title.js

--- src/String/Easy/168 - Excel Sheet Column Title.js
+++ src/String/Easy/168 - Excel Sheet Column Title.js
@@ -2,6 +2,21 @@
  * @param {number} n
  * @return {string}
  */
-var convertToTitle = function(n) {
-    
+
+/*----- solution 1 -----
+Convert from base-10 to base-26
+Keep dividing n by 26 until n is not greater than 0, 
+The remainder queue is the result.
+Time complexity is O(n^2)
+Space complexity is O(n)
+*/
+var convertToTitle = function (n) {
+  let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+    result = [];
+  while (n > 0) {
+    n -= 1;
+    result.unshift(letters[n % 26]);
+    n = Math.floor(n / 26);
+  }
+  return result.join('');
 };
