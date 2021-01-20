1c4009f5bf8b0b084e2b6a9db4264c7d66ea0fcc
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-20 11:58:30 +1100

    Delete 0007 - Reverse Integer.js

--- src/0007 - Reverse Integer.js
+++
@@ -1,9 +0,0 @@
-var reverse = function(x) {
-
-    //using array.reverse to reverse the number
-    //convert to array then back to number
-    let result = Math.abs(x).toString().split('').reverse().join('')*Math.sign(x);
-    //check if overflow
-    return (result < -(2**31) || result >= 2**31) ? 0 : result;
-    
-};
