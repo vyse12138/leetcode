a2848968c1be6293650cd2dc037befba28d06c06
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-28 12:29:30 +1100

    Update 0069 - Sqrt(x).js

--- src/Math/Easy/0069 - Sqrt(x).js
+++ src/Math/Easy/0069 - Sqrt(x).js
@@ -4,10 +4,21 @@
  */
 
 /*----- solution 1 -----
-TLE 
-Time complexity is O(n)
+Binary search
+Time complexity is O(log(n)
 Space complexity is O(1)
 */
-var mySqrt = function(x) {
-    
+var mySqrt = function (x) {
+  let left = 0, right = x, mid, result = -1;
+  while (left <= right) {
+    mid = parseInt((left + right) / 2);
+    if (mid ** 2 <= x) {
+      result = mid;
+      left = mid + 1;
+    }
+    else {
+      right = mid - 1;
+    }
+  }
+  return result;
 };
