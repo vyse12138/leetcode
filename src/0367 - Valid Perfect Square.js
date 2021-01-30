06d3a60006069fb648206f5d2fe8db366bef6054
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-01-30 12:24:06 +1100

    rename

--- src/0367 - Valid Perfect Square.js
+++ src/0367 - Valid Perfect Square.js
@@ -4,7 +4,8 @@
  */
 
 /*----- solution 1 -----
-Binary search
+Binary search to find closest integer sqrt of num, 
+then check if the result^2 is equal to num.
 Time complexity is O(log(n))
 Space complexity is O(1)
 */
@@ -20,6 +21,6 @@ var isPerfectSquare = function(num) {
       right = mid - 1;
     }
   }
-  return result;
+  return result**2 == num;
 };
 
