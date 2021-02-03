684c3517f9929b00ab4b08391b29b894a58cd930
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-03 13:25:59 +1100

    Create 0257-binaryTreePaths.js

---
+++ src/0257-binaryTreePaths.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+
+/*----- solution 1 -----
+Recursion, DFS.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var binaryTreePaths = function (root) {
+  const result = [], buildPath = (node, path) => {
+    if (node) {
+      path += node.val.toString();
+      if (!node.left && !node.right) {
+        result.push(path);
+      }
+      else {
+        path += "->";
+        buildPath(node.left, path);
+        buildPath(node.right, path);
+      }
+    }
+  }
+  buildPath(root, '');
+  return result;
+};
