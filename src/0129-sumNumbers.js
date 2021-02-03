4e21e4e2dfcf6c05300f843ac020716254b46158
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-03 16:29:18 +1100

    Create 0129-sumNumbers.js

---
+++ src/0129-sumNumbers.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+/*----- solution 1 -----
+Recursion, DFS.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var sumNumbers = function (root) {
+  let result = 0, dfs = (node, sum) => {
+    if (node) {
+      sum += node.val;
+      if (!node.left && !node.right) {
+        result += sum - 0;
+      }
+      else {
+        dfs(node.left, sum);
+        dfs(node.right, sum);
+      }
+    }
+  }
+  dfs(root, '');
+  return result;
+};
