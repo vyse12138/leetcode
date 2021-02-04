37f13d89b36d601e2772fdd32af2d83d00260eb5
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-04 13:56:00 +1100

    Update 0104-maxDepth.js

--- src/0104-maxDepth.js
+++ src/0104-maxDepth.js
@@ -42,5 +42,5 @@ var maxDepth = function(root) {
   if (!root) {
     return 0;
   }
-  return max(maxDepth(root.left), maxDepth(root.right)) + 1;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
