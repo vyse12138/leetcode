eaa34a5ad02c71f296ceb92165f2c6c0cee96a45
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-04 13:33:40 +1100

    rename

---
+++ src/0104-maxDepth.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+
+/*----- solution 1 -----
+Recursion, DFS.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var maxDepth = function(root) {
+  let depth = 0, dfs = (root, currentDepth) => {
+    if (root) {
+      currentDepth++;
+      if (!root.left && !root.right) {
+        depth = Math.max(depth, currentDepth);
+      }
+      else {
+        dfs(root.left, currentDepth);
+        dfs(root,right,currentDepth);
+      }
+    }
+  }
+  dfs(root, 0);
+  return depth;
+};
+
+/*----- solution 2 -----
+Recursion, DFS.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var maxDepth = function(root) {
+  if (!root) {
+    return 0;
+  }
+  return max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
