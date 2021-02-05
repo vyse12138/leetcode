bda4debc2a3238654499bc08133152de0d2578d8
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-06 08:15:57 +1100

    Create 0020-isValid.js

---
+++ src/0020-isValid.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+/*----- solution 1 -----
+Using stack to store.
+Time complexity is O(n)
+Space complexity is O(n)
+*/
+var isValid = function (s) {
+  let map = new Map([
+    [')', '('],
+    [']', '['],
+    ['}', '{']
+  ]);
+  let stack = [];
+  s = s.split('');
+  while(s.length) {
+    let current = s.shift();
+    if (map.has(current) && map.get(current) === stack[stack.length-1]) {
+      stack.pop();
+    }
+    else {
+      stack.push(current);
+    }
+  }
+  return !stack.length;
+};
