aeae143b8fd7f55ec21520527d1fb067cd51a8a3
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-05 22:59:17 +1100

    rename

--- src/0059-generateMatrix.js
+++ src/0059-generateMatrix.js
@@ -9,35 +9,32 @@ iterate in the same way
 Time complexity is O(n^2)
 Space complexity is O(1)
 */
-var generateMatrix = function(n) {
-    let 
+var generateMatrix = function (n) {
+    let
         left = 0,
         right = n - 1,
         top = 0,
         bottom = n - 1,
         result = new Array(n),
         num = 1;
-
-    for (let i = 0; i < n; i++){
+    for (let i = 0; i < n; i++) {
         result[i] = new Array(n);
     }
-
-    while (num <= n*n){
-        for (let i = left; i <= right; i++){
+    while (num <= n * n) {
+        for (let i = left; i <= right; i++) {
             result[top][i] = num++;
         }
-        for (let i = top+1; i <= bottom; i++){
+
+        for (let i = top + 1; i <= bottom; i++) {
             result[i][right] = num++;
         }
-        for (let i = right-1; i >= left; i--){
+        for (let i = right - 1; i > left; i--) {
             result[bottom][i] = num++;
         }
-        for (let i = bottom; i >= top; i--){
+        for (let i = bottom; i > top; i--) {
             result[i][left] = num++;
         }
-
-        left++;
-        right--;
+        left++; right--;
         top++;
         bottom--;
     }
