0815762433af220e1b88478476658bd93fe5a7e4
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-05 22:36:33 +1100

    Create 0199-rightSideView.js

---
+++ src/0199-rightSideView.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+/*----- solution 1 -----
+Iteration, BFS.
+Using queue.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var rightSideView = function(root) {
+  if (!root) {
+    return [];
+  }
+  let result = [], queue = [root];
+  while(queue.length) {
+    let length = queue.length, right = 0;
+    while (length--) {
+      let node = queue.shift();
+      if (length === 0) {
+        right = node.val;
+      }
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    result.push(right);
+  }
+  return result;
+};
