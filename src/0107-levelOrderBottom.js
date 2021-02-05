0d343be0c963eebf4c526e65125555429d4d247e
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-05 22:05:57 +1100

    Create 0107-levelOrderBottom.js

---
+++ src/0107-levelOrderBottom.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+
+/*----- solution 1 -----
+Iteration, BFS.
+Using queue.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var levelOrderBottom = function(root) {
+  let result = [], queue = [];
+  if (!root) {
+    return [];
+  }
+  queue.push(root);
+  while(queue.length) {
+    let length = queue.length, row = [];
+    while(length--) {
+      let node = queue.pop()
+      row.push(node.val);
+      node.left && queue.unshift(node.left);
+      node.right && queue.unshift(node.right);
+    }
+    result.unshift(row);
+  }
+  return result;
+};
