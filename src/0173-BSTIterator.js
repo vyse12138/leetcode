c40ec01d9a2ca58fa29ffac958ef8f8da13e02c7
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-06 19:08:29 +1100

    Update 0173-BSTIterator.js

---
+++ src/0173-BSTIterator.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+
+/*----- solution 1 -----
+Recursion. Using array to store inorder dfs values.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var BSTIterator = function (root) {
+  this.array = [];
+  let dfs = (node) => {
+    if (!node) {
+      return;
+    }
+    dfs(node.left);
+    this.array.push(node.val);
+    dfs(node.right);
+  }
+  dfs(root);
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function () {
+  return this.array.shift();
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function () {
+  return this.array.length;
+
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
