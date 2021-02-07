99b5b7d29d82bd361ed8e4706ef88d60b87dd682
Vyse12138 <62495388+Vyse12138@users.noreply.github.com>
2021-02-07 19:19:41 +1100

    Create 0116-connect.js

---
+++ src/0116-connect.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+
+/*----- solution 1 -----
+BFS. Queue.
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
+var connect = function (root) {
+  if (!root) {
+    return null;
+  }
+  let queue = [root];
+  while (queue.length) {
+    let length = queue.length;
+    while (length--) {
+      let node = queue.shift();
+      if (length !== 0) {
+        node.next = queue[0];
+      }
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+  }
+  return root;
+};
