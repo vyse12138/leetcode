19e1639cdc5434178dacb0fd5b7537d2805e5b06
Jake Zhu <zhuyulei12333@gmail.com>
2021-02-17 13:23:40 +1100

    Update 0007-reverse.js

--- src/0007-reverse.js
+++ src/0007-reverse.js
@@ -9,12 +9,12 @@ Time complexity is O(n)
 Space complexity is O(1)
 */
 
-var reverse = function(x) {
+var reverse = function (x) {
 
     //using array.reverse to reverse the number
     //convert to array then back to number
-    let result = Math.abs(x).toString().split('').reverse().join('')*Math.sign(x);
+    let result = Math.abs(x).toString().split('').reverse().join('') * Math.sign(x);
     //check if overflow
-    return (result < -(2**31) || result >= 2**31) ? 0 : result;
-    
-};
+    return (result < -(2 ** 31) || result >= 2 ** 31) ? 0 : result;
+
+}; 
