f364f5aefd7d57cfe048b59358e867fcf3f084a7
unknown <zhuyulei12333@gmail.com>
2021-03-04 12:05:55 +1100

    Create 0354-maxEnvelopes.js

---
+++ src/0354-maxEnvelopes.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[][]} envelopes
+ * @return {number}
+ */
+
+/*----- solution 1 -----
+Using js API
+Time complexity is O(n)
+Space complexity is O(1)
+*/
+var maxEnvelopes = function(envelopes) {
+  
+};
