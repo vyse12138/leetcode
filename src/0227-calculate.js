ac51b42a97186b04abfdb76bc0c8b90a78ef1f16
Vyse12138 <zhuyulei12333@gmail.com>
2021-03-11 21:28:49 +1100

    Update 0227-calculate.js

--- src/0227-calculate.js
+++ src/0227-calculate.js
@@ -2,6 +2,42 @@
  * @param {string} s
  * @return {number}
  */
+
+/*----- solution 1 -----
+Stack
+Time complexity is O(n) 
+Space complexity is O(n)
+*/
 var calculate = function (s) {
-  let stack = [];
+  s += "+";
+  let stack = [], sign = "+",num = 0;
+  for (char of s) {
+    if (char !== " ") {
+      if (!isNaN(char - 0)) {
+        num = char - 0 + num * 10;
+      } else {
+        switch (sign) {
+          case "+": {
+            stack.push(num);
+            break;
+          }
+          case "-": {
+            stack.push(-num);
+            break;
+          }
+          case "*": {
+            stack.push(stack.pop() * num);
+            break;
+          }
+          case "/": {
+            stack.push((stack.pop() / num) | 0);
+            break;
+          }
+        }
+        sign = char;
+        num = 0;
+      }
+    }
+  }
+  return stack.reduce((a, b) => a + b);
 };
